127849e45788d11f86b079476dd565cf5af78dd2
Josh Payne <snare117@gmail.com>
2019-02-19 16:29:14 -0800

    using brute force for generating possbilities

--- sidon_script.py
+++ sidon_script.py
@@ -1,4 +1,6 @@
-size = 5000
+import itertools
+
+size = 20
 
 '''
 This function uses the Sieve of Eratosthenes method to find all primes under n and returns the largest one.
@@ -15,11 +17,11 @@ def largest_prime_below(n):
 				j+=1
 	return primes.pop()
     
-erdos_turan_bound = lambda n: int(n**(0.5)+2*(n**(0.25)+10))
+erdos_turan_bound = lambda n: int((n**(0.5))+(2*(n**(0.25)))+10)
 
 # to check
-# print(erdos_turan_bound(size))
-# print(largest_prime_below(int(size**(0.5))))
+print(erdos_turan_bound(size))
+print(largest_prime_below(int(size**(0.5))))
 
 '''
 Here, we keep track of all possible sums in A, returning false if there are any duplicates, and true otherwise.
@@ -38,15 +40,19 @@ from class: largest prime below square root of N < max sidon set size below N < 
 This tests all integers in that range in reversed order to find the largest Sidon Set.
 '''
 def largestSidonSet(N):
-	for n in reversed(range(largest_prime_below(N),erdos_turan_bound(N))):
+	print("largest prime below square root of N: ",largest_prime_below(int(size**(0.5))),"Erdos-Turan bound for N: ",min(erdos_turan_bound(N),N))
+	for n in reversed(range(largest_prime_below(int(size**(0.5))),min(erdos_turan_bound(N),N))):
+		print(n)
 		for A in makeSidonSets(n,N):
 			if foundSidonSet(A):
-				return n
+				return "size: ", n, A
+	return "didn't find any"
 '''
 makes possible Sidon sets of size n from N.
 '''
 def makeSidonSets(n,N):
+	print("sidonSets",n,N)
 	# how can we efficiently generate all the potential Sidon Sets of size n from N? Would we really need to generate N choose n sets?
-	return[[]]
+	return list(itertools.combinations(range(N),n))
 
-print(largestSidonSet(size))
+print("largest found: ",largestSidonSet(size))
